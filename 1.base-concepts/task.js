6a2c727674846259fe8fef6918852a1c67cd8295
Zelenkova <e.valerevna.turova@yandex.ru>
2023-05-23 16:14:55 +0200

    First task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+  const d = b**2 - 4*a*c;
+  if(d > 0) {
+    let x1 = (-b + Math.sqrt(d) )/(2*a);
+    let x2 = (-b - Math.sqrt(d) )/(2*a);
+
+    return arr = [x1, x2]
+  } else if(d === 0) {
+   let x = -b/(2*a);
+
+   return arr = [x];
+  } else {
+    return arr = [];
+  }
+  
   
   return arr;
 }
