f2f3062dcc11dce879f660a30b412e14b5e75e04
Zelenkova <e.valerevna.turova@yandex.ru>
2023-05-24 13:17:07 +0200

    All first tasks

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,24 +2,44 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  const d = b**2 - 4*a*c;
-  if(d > 0) {
-    let x1 = (-b + Math.sqrt(d) )/(2*a);
-    let x2 = (-b - Math.sqrt(d) )/(2*a);
+  const d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
 
-    return arr = [x1, x2]
-  } else if(d === 0) {
-   let x = -b/(2*a);
+    return (arr = [x1, x2]);
+  } else if (d === 0) {
+    let x = -b / (2 * a);
 
-   return arr = [x];
+    return (arr = [x]);
   } else {
-    return arr = [];
+    return (arr = []);
   }
-  
-  
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = +percent;
+  contribution = +contribution;
+  amount = +amount;
+  countMonths = +countMonths;
+
+  if (
+    Number.isNaN(percent) ||
+    Number.isNaN(contribution) ||
+    Number.isNaN(amount) ||
+    Number.isNaN(countMonths)
+  ) {
+    return false;
+  }
+
+  let P = percent / 100 / 12;
+  let S = amount - contribution;
+  let n = countMonths;
+  let payment = S * (P + P / ((1 + P) ** n - 1));
+  let totalAmount = payment * countMonths;
+  totalAmount = +totalAmount.toFixed(2);
+
+  return totalAmount;
 }
