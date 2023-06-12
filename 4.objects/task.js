f8454bbdf116632642ec288bfd5d87410b6fccd3
Zelenkova <e.valerevna.turova@yandex.ru>
2023-06-12 14:01:43 +0300

    Third work is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  let student1 = new Student("Bill", "male", 20);
+  let student2 = new Student("Anya", "female", 20);
+  let student3 = new Student("Yma", "female", 22);
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  };
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.excluded) {
+      return;
+    }
+    this.marks.push(...marksToAdd);
+  };
   
-}
-
-Student.prototype.exclude = function (reason) {
+  Student.prototype.getAverage = function () {
+    let result =
+      this.marks && this.marks.length
+        ? this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length
+        : 0;
+    return result;
+  };
   
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  };
+  
